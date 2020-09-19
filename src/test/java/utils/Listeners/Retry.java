3e744986ebc6e2470f90197a06ba62f76cdd1976
Hrishikesh <hbnair080@gmail.com>
2020-09-19 22:19:30 +0530

    Increased retry

--- src/test/java/utils/Listeners/Retry.java
+++ src/test/java/utils/Listeners/Retry.java
@@ -14,7 +14,7 @@ import utils.ExtentReports.ExtentTestManager;
 public class Retry implements IRetryAnalyzer {
 
     private int count = 0;
-    private static int maxTry = 0; //Run the failed test 2 times
+    private static int maxTry = 2; //Run the failed test 2 times
 
     @Override
     public boolean retry(ITestResult iTestResult) {
